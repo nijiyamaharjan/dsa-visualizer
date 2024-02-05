72104e536938e06d5b96b5d1022549e266d82d1d
Rubika Bashyal <bashyalrubika@gmail.com>
2024-02-05 21:56:49 +0545

    graph-visualizer

--- dsa-visualizer/src/components/GraphAlgorithm.js
+++ dsa-visualizer/src/components/GraphAlgorithm.js
@@ -1,101 +1,83 @@
-import React, { Component } from 'react'
-import {NavLink} from 'react-router-dom'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import './graph.css'
+import './home.css';
 
 export class GraphAlgorithm extends Component {
     render() {
         return (
-            <center>
-            <div>
-            <Helmet>
-        <title>Graph Algorithms</title>
-      </Helmet>
-            <br/>
-                <h1 className="heading" style={{color:'darkblue'}}>
-                    GRAPH ALGORITHM VISUALISER
+            <div className="min-h-screen flex flex-col items-center gradient-container">
+                <Helmet>
+                    <title>Graph Algorithms</title>
+                </Helmet>
+                <br />
+                <h1 className="text-4xl font-bold text-gray-700 mb-10 mt-10 heading">
+                    GRAPH ALGORITHM VISUALIZER
                 </h1>
-                <br/>
-                <div className="container">
-                    <div className="row">
-                    
-                    <div className="col-sm-12 col-md-3">
-                    <center>
-                    <div className="card box" style={{width: '100%',backgroundColor:'#fafafa'}}>
-                    
-                    <div className="card-body">
-                        <h3 className="card-title" style={{fontFamily:'Georgia'}}><br/>Breadth First and Depth First Algorithms</h3>
-                        <p style={{fontFamily: 'Georgia'}} className="card-text">A simple simulation of Breadth First Traversal and Depth First traversal on an undirected graph created by the user.</p>
-                        <center>
-                        <NavLink to='/bfsdfs' style={{width:'70%',color:'white',fontFamily:'Georgia'}} className="button button4">BFS and DFS</NavLink>
-                        </center>
-                    </div>
-                    </div>
-                    </center>
-                    </div>
-    
-                    <div className="col-sm-12 col-md-3">
-                    <center>
-                    <div className="card box" style={{width: '100%',backgroundColor:'#fafafa'}}>
-                    
-                    <div className="card-body">
-                        <h3 className="card-title" style={{fontFamily:'Georgia'}}><br/>Dijkstra's Shortest Path Algorithm</h3>
-                        <p style={{fontFamily: 'Georgia'}} className="card-text">A simulation of Djikstra's Shortest Path Algorithm and finding the shortest paths from the chosen source vertex to all the nodes.</p>
-                        <center>
-                        <NavLink to='/dijkstra' style={{width:'70%',color:'white',fontFamily:'Georgia'}} className="button button4">Dijkstra's Algorithm</NavLink>
-                        </center>
-                    </div>
-                    </div>
-                    </center>
-                    </div>
+                <br />
+                <div className="container mt-20">
+                    <div className="flex flex-wrap justify-center">
+                        <div className="w-full md:w-1/4 mb-6 p-4">
+                            <div className="w-full max-w-md bg-white bg-opacity-50 rounded-md shadow-xl flex flex-col items-center card-wrapper">
+                                <div className="card-body p-6">
+                                    <h3 className="card-title mb-4 text-lg text-white">
+                                        Breadth First and Depth First Algorithms
+                                    </h3>
+                                    <p className="card-text font-serif text-white mb-4">
+                                        A simple simulation of Breadth First Traversal and Depth First traversal on an undirected graph created by the user.
+                                    </p>
+                                    <NavLink to='/bfsdfs' className="p-2 text-white bg-gray-700 hover:bg-black focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">BFS and DFS</NavLink>
+                                </div>
+                            </div>
+                        </div>
 
-                    <div className="col-sm-12 col-md-3">
-                    <center>
-                    <div className="card box" style={{width: '100%',backgroundColor:'#fafafa'}}>
-                    
-                    <div className="card-body">
-                        <h3 className="card-title" style={{fontFamily:'Georgia'}}><br/>Kruskal's Minimal Spanning Tree</h3>
-                        <p style={{fontFamily: 'Georgia'}} className="card-text">A simple simulation Kruskal's Algorithm for finding the Minimal Spanning Tree of a connected undirected weighted graph.</p>
-                        <center>
-                        <NavLink to='/KruskalMST' style={{width:'70%',color:'white',fontFamily:'Georgia'}} className="button button4">Kruskal's MST</NavLink>
-                        </center>
-                    </div>
-                    </div>
-                    </center>
-                    </div>
+                        <div className="w-full md:w-1/4 mb-4 p-4">
+                            <div className=" w-full max-w-md bg-white bg-opacity-50 rounded-md shadow-xl flex flex-col items-center card-wrapper">
+                                <div className="card-body p-6">
+                                    <h3 className="card-title mb-4 text-lg text-white">
+                                        Dijkstra's Shortest Path Algorithm
+                                    </h3>
+                                    <p className="card-text font-serif text-white mb-4">
+                                        A simulation of Djikstra's Shortest Path Algorithm and finding the shortest paths from the chosen source vertex to all the nodes.
+                                    </p>
+                                    <NavLink to='/dijkstra' className="p-2 text-white bg-gray-700 hover:bg-black focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Dijkstra's Algorithm</NavLink>
+                                </div>
+                            </div>
+                        </div>
 
-                    <div className="col-sm-12 col-md-3">
-                    <center>
-                    <div className="card box" style={{width: '100%',backgroundColor:'#fafafa'}}>
-                    
-                    <div className="card-body">
-                        <h3 className="card-title" style={{fontFamily:'Georgia'}}><br/>Prim's Minimal Spanning Tree Algorithm</h3>
-                        <p style={{fontFamily: 'Georgia'}} className="card-text">A simple simulation Prim's Algorithm for finding the Minimal Spanning Tree of a connected undirected weighted graph.</p>
-                        <center>
-                        <NavLink to='/PrimMST' style={{width:'70%',color:'white',fontFamily:'Georgia'}} className="button button4">Prim's MST</NavLink>
-                        </center>
-                    </div>
-                    </div>
-                    </center>
-                    </div>
+                        <div className="w-full md:w-1/4 mb-4 p-4">
+                            <div className=" w-full max-w-md bg-white bg-opacity-50 rounded-md shadow-xl flex flex-col items-center card-wrapper">
+                                <div className="card-body p-6">
+                                    <h3 className="card-title mb-4 text-lg text-white">
+                                        Kruskal's Minimal Spanning Tree
+                                    </h3>
+                                    <p className="card-text font-serif text-white mb-4">
+                                        A simple simulation Kruskal's Algorithm for finding the Minimal Spanning Tree of a connected undirected weighted graph.
+                                    </p>
+                                    <NavLink to='/KruskalMST' className="p-2 text-white bg-gray-700 hover:bg-black focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Kruskal's MST</NavLink>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-full md:w-1/4 mb-4 p-4">
+                            <div className="w-full max-w-md bg-white bg-opacity-50 rounded-md shadow-xl flex flex-col items-center card-wrapper">
+                                <div className="card-body p-6">
+                                    <h3 className="card-title mb-4 text-lg text-white">
+                                        Prim's Minimal Spanning Tree Algorithm
+                                    </h3>
+                                    <p className="card-text font-serif text-white mb-4">
+                                        A simple simulation Prim's Algorithm for finding the Minimal Spanning Tree of a connected undirected weighted graph.
+                                    </p>
+                                    <NavLink to='/PrimMST' className="p-2 text-white bg-gray-700 hover:bg-black focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Prim's MST</NavLink>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
-                </div>
-
-                <div className="row">
-                <div className="col-12">
-                <br/><br/>
-                </div>
-                </div>
-                <div className="row">
-                <div className="col-12" style={{backgroundColor:'#fafafa',border:'.5px solid #d6d6d6',borderRadius:'4px',padding:'10px 10px 0px 10px'}}>
-                
-                </div>
                 </div>
             </div>
-            </center>
-        )
+        );
     }
 }
 
-export default GraphAlgorithm
+export default GraphAlgorithm;
