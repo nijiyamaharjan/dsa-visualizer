03fc9f7aba5b12de68af96f41a0f2a59719c16c2
nijiyamaharjan <nijiyamn@gmail.com>
2024-04-20 13:00:51 +0545

    Update App.js

--- dsa-visualizer/src/App.js
+++ dsa-visualizer/src/App.js
@@ -2,16 +2,16 @@
 import React from 'react';
 import './App.css';
 import MainComponent from './MainComponent';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter base="/">
       <Routes>
         {/* <Route path='/*' element={<MainComponent />} /> */}
         <Route exact path='/' element={<MainComponent />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
